1a44c5d4bd2e74f9a0712728d35a9bccef377db6
truepurpledino <75775263+truepurpledino@users.noreply.github.com>
2025-08-05 18:17:26 -0400

    added recipes for sophisticated storage containers

--- kubejs/server_scripts/mods/sophisticatedstorage.js
+++ kubejs/server_scripts/mods/sophisticatedstorage.js
@@ -11,6 +11,51 @@ ServerEvents.recipes(event => {
     // Remove Limited barrels
     event.remove({ id: /^sophisticatedstorage:.*limited.+barrel.+$/ })
     event.remove({ output: /^sophisticatedstorage:limited_barrel.+$/ })
+    
+    const sophStorageMaterials = [
+        ["", null, null],
+        ["copper_", "copper"],
+        ["iron_", "iron"],
+        ["gold_", "gold"],
+        ["diamond_", "diamond"]
+    ]
+
+    const storageContainers = [
+        "chest",
+        "barrel",
+        "shulker_box"
+    ]
+
+    sophStorageMaterials.forEach((material, index) => {
+        if (index == 0) return;
+
+        storageContainers.forEach(container => {
+            let outputStorage = `sophisticatedstorage:${material[0]}${container}`
+            let inputStorage = `sophisticatedstorage:${sophStorageMaterials[index - 1][0]}${container}`
+            event.remove({ mod: "sophisticatedstorage", output: outputStorage })
+            event.custom({
+                "type": "sophisticatedstorage:storage_tier_upgrade",
+                "key": {
+                    "N": {
+                        "tag": (`c:plates/${material[1]}`)
+                    },
+                    "C": {
+                        "item": inputStorage
+                    }
+                },
+                "pattern": [
+                    "NNN",
+                    "NCN",
+                    "NNN"
+                ],
+                "result": {
+                    "count": 1,
+                    "id": outputStorage
+                }
+            }).id(st(`${material[0]}${container}_upgrade`));
+        });
+    });
+
 })
 
 
