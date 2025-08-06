685d3750e9dfbb166fb6fdbe268572c893706c4f
truepurpledino <75775263+truepurpledino@users.noreply.github.com>
2025-08-05 20:52:56 -0400

    initial sophisticated upgrades revamp

--- kubejs/server_scripts/mods/sophisticatedbackpacks.js
+++ kubejs/server_scripts/mods/sophisticatedbackpacks.js
@@ -6,7 +6,74 @@
 ServerEvents.recipes(event => {
     // -- MOD NAMESPACE UTILITY FUNCTIONS -- // 
     let st = (id) => `statech:sophisticatedbackpacks/${id}`;
-    
+
+    const SOPH_DELETED = [
+        sb('stack_upgrade_omega_tier'),
+        sb('compacting_upgrade'),
+        ss('backpack_compacting_upgrade_from_storage_compacting_upgrade'),
+        sb('advanced_compacting_upgrade'),
+        ss('backpack_advanced_compacting_upgrade_from_storage_advanced_compacting_upgrade'),
+        sb('pump_upgrade'),
+        sb('battery_upgrade'),
+        sb('magnet_upgrade'),
+        sb('backpack_magnet_upgrade_from_storage_magnet_upgrade'),
+        sb('advanced_magnet_upgrade'),
+        sb('backpack_advanced_magnet_upgrade_from_storage_advanced_magnet_upgrade'),
+        sb('advanced_pump_upgrade'),
+        sb('feeding_upgrade'),
+        sb('upgrade_base')
+    ];
+    SOPH_DELETED.forEach(id => event.remove({ id: id })); 
+
+    // -- UPGRADE BASE -- //
+    event.shaped(sb('upgrade_base'), [
+        'SIS',
+        'ILI',
+        'SIS'
+    ],
+    {
+        S: mc('string'),
+        I: 'c:plates/bronze',
+        L: mc('leather')
+    })
+    .id(st('upgrade_base')); 
+
+    // -- FEEDING UPGRADE -- //
+    event.shaped(sb('feeding_upgrade'), [
+        ' C ',
+        'ABM',
+        ' F '
+    ],
+    {
+        C: mc('golden_carrot'),
+        D: sb('upgrade_base'),
+        M: mc('glistering_melon_slice'),
+        G: mc('golden_apple'),
+        F: ei('robot_auto_feeder')
+    })
+    .id(st('feeding_upgrade')); 
+
+    // -- MAGNET UPGRADE -- //
+    event.shaped(sb('magnet_upgrade'), [
+        'D',
+        'M'
+    ],
+    {
+        D: sb('pickup_upgrade'),
+        M: sm('basicmagnet')
+    })
+    .id(st('magnet_upgrade')); 
+
+    // -- ADVANCED MAGNET UPGRADE -- //
+    event.shaped(sb('advanced_magnet_upgrade'), [
+        'D',
+        'M'
+    ],
+    {
+        D: sb('magnet_upgrade'),
+        M: sm('advancedmagnet')
+    })
+    .id(st('advanced_magnet_upgrade'));   
  
     // Adapted from Monifactory scripts, see https://github.com/ThePansmith/Monifactory/blob/main/kubejs/server_scripts/mods/Sophisticated_Storagevent.js
 
--- kubejs/server_scripts/mods/sophisticatedstorage.js
+++ kubejs/server_scripts/mods/sophisticatedstorage.js
@@ -5,13 +5,32 @@
 ServerEvents.recipes(event => {
     // -- MOD NAMESPACE UTILITY FUNCTIONS -- // 
     let st = (id) => `statech:sophisticatedstorage${id}`;
-    
+ 
+    const SOPH_DELETED = [
+        ss('stack_upgrade_omega_tier'),
+        ss('compression_upgrade'),
+        ss('compacting_upgrade'),
+        ss('storage_compacting_upgrade_from_backpack_compacting_upgrade'),
+        ss('advanced_compacting_upgrade'),
+        ss('storage_advanced_compacting_upgrade_from_backpack_advanced_compacting_upgrade'),
+        ss('magnet_upgrade'),
+        ss('storage_magnet_upgrade_from_backpack_magnet_upgrade'),
+        ss('advanced_magnet_upgrade_from_basic'),
+        ss('storage_advanced_magnet_upgrade_from_backpack_advanced_magnet_upgrade'),
+        ss('pump_upgrade'),
+        ss('xp_pump_upgrade'),
+        ss('advanced_pump_upgrade'),
+        ss('feeding_upgrade')
+    ];
+    SOPH_DELETED.forEach(id => event.remove({ id: id })); 
+
+
     // Adapted from Monifactory scripts, see https://github.com/ThePansmith/Monifactory/blob/main/kubejs/server_scripts/mods/Sophisticated_Storagevent.js
 
     // Remove Limited barrels
     event.remove({ id: /^sophisticatedstorage:.*limited.+barrel.+$/ })
     event.remove({ output: /^sophisticatedstorage:limited_barrel.+$/ })
-    
+
     const sophStorageMaterials = [
         ["", null, null],
         ["copper_", "copper"],
