c354425eff2293695010314e36d8105fcad6505c
truepurpledino <75775263+truepurpledino@users.noreply.github.com>
2026-03-29 00:35:50 -0400

    fix bulk compactor, add blast alloy smelter multi and fix mod load order for datagen

--- kubejs/startup_scripts/modern_industrialization/machine_casing.js
+++ kubejs/startup_scripts/modern_industrialization/machine_casing.js
@@ -10,5 +10,9 @@ MIMachineEvents.registerCasings(event => {
 	event.registerBlockImitation('chemically_inert_ptfe_casing', 'modern_industrialization:chemically_inert_ptfe_casing');
 	event.registerBlockImitation('tungstensteel_machine_casing', 'modern_industrialization:tungstensteel_machine_casing');
 	event.registerBlockImitation('sky_steel_machine_casing', 'megacells:sky_steel_block');
+	event.registerBlockImitation('enderium_machine_casing', 'modern_industrialization:enderium_machine_casing');
+	event.registerBlockImitation('steel_plated_bricks_casing', 'extended_industrialization:steel_plated_bricks');
+	event.registerBlockImitation('korp4_machine_casing', 'xtonesreworked:korp_block_4');
+	event.registerBlockImitation('korp9_machine_casing', 'xtonesreworked:korp_block_9');
 });
 
